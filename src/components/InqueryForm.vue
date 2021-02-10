6ce4c86280be3a9389bd97ee66417c5eb3d481c9
Anisur Rahman <anisurlist3@gmail.com>
2021-02-10 16:28:13 +0600

    completed search component top part

--- src/components/InqueryForm.vue
+++ src/components/InqueryForm.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex flex-col w-full p-2 space-y-3 border-b border-gray-300">
+  <div
+    class="flex flex-col w-full bg-gray-50 p-2 space-y-3 border-b border-gray-300"
+  >
     <div class="flex items-center justify-between">
       <div class="flex items-center">
         <div class="flex items-center">
           <input
             type="checkbox"
-            class="p-0 pl-1 text-gray-600 border border-white focus:ring-1 focus:ring-gray-600 rounded"
+            class="p-0 pl-1 text-gray-600 border focus:ring-1 focus:ring-gray-600 rounded"
             id="checkdate"
             v-model="toggleDate"
           />
@@ -16,9 +18,10 @@
         <div class="flex items-center">
           <input
             class="p-0 pl-1 border rounded"
-            :class="toggleDate ? null : 'opacity-5'"
+            :class="toggleDate ? null : 'opacity-50'"
             type="date"
             name="form"
+            v-model="toDate"
             id="form"
             :disabled="toggleDate ? false : true"
           />
@@ -27,17 +30,56 @@
         <div class="flex items-center">
           <input
             class="p-0 pl-1 border rounded"
-            :class="toggleDate ? null : 'opacity-5'"
+            :class="toggleDate ? null : 'opacity-50'"
             type="date"
+            v-model="fromDate"
             name="form"
             id="form"
             :disabled="toggleDate ? false : true"
           />
         </div>
       </div>
+    </div>
+    <div class="flex items-center justify-between">
+      <div class="flex items-center">
+        <div class="flex items-center">
+          <input
+            type="checkbox"
+            class="p-0 pl-1 text-gray-600 border focus:ring-1 focus:ring-gray-600 rounded"
+            id="checkdatetime"
+            v-model="toggleDateTime"
+          />
+          <label class="ml-1 mr-2 font-sans font-medium" for="checkdatetime"
+            >Tag Time</label
+          >
+        </div>
+        <div class="flex items-center">
+          <input
+            class="flex-1 p-0 pl-1 border rounded"
+            :class="toggleDateTime ? null : 'opacity-50'"
+            type="datetime-local"
+            name="form"
+            v-model="toDateTime"
+            id="form"
+            :disabled="toggleDateTime ? false : true"
+          />
+        </div>
+        <div>-</div>
+        <div class="flex items-center">
+          <input
+            class="flex-1 p-0 pl-1 border rounded"
+            :class="toggleDateTime ? null : 'opacity-50'"
+            type="datetime-local"
+            name="form"
+            id="form"
+            v-model="fromDateTime"
+            :disabled="toggleDateTime ? false : true"
+          />
+        </div>
+      </div>
       <div class="flex items-center justify-between">
         <button
-          class="px-3 py-1 ml-5 text-sm text-black bg-purple-300 rounded-md shadow"
+          class="px-3 py-1 ml-5 text-sm text-blac bg-purple-300 rounded-md shadow"
         >
           Display
         </button>
@@ -53,117 +95,78 @@
         </button>
       </div>
     </div>
-    <div class="flex items-center">
+    <div class="flex items-center space-x-2">
       <div class="flex items-center">
-        <input
-          type="checkbox"
-          class="rounded-sm"
-          id="checkdatetime"
-          v-model="toggleDateTime"
-        />
-        <label class="ml-1 mr-2 font-sans font-medium" for="checkdatetime"
-          >Tag Time</label
+        <label class="font-semibold" for="sslno">SSL No: </label>
+        <select
+          class="ml-2 py-0 pl-3 pr-9 border border-gray-600 focus:ring-1 focus:ring-gray-600 rounded"
+          name="sslno"
+          id="sslno"
         >
+          <option value="">Any SSL</option>
+          <option value="">124</option>
+          <option value="">125</option>
+          <option value="">126</option>
+        </select>
       </div>
       <div class="flex items-center">
-        <input
-          class="flex-1 p-0 pl-1 border rounded"
-          :class="toggleDateTime ? null : 'opacity-5'"
-          type="datetime-local"
-          name="form"
-          id="form"
-          :disabled="toggleDateTime ? false : true"
-        />
+        <label class="font-semibold" for="floor">Floor: </label>
+        <select
+          class="ml-2 py-0 pl-3 pr-9 border border-gray-600 focus:ring-1 focus:ring-gray-600 rounded"
+          name="sslno"
+          id="sslno"
+        >
+          <option value="">Any floor...</option>
+          <option value="">Floor-1</option>
+          <option value="">Floor-2</option>
+        </select>
       </div>
-      <div>-</div>
       <div class="flex items-center">
-        <input
-          class="flex-1 p-0 pl-1 border rounded"
-          :class="toggleDateTime ? null : 'opacity-5'"
-          type="datetime-local"
-          name="form"
-          id="form"
-          :disabled="toggleDateTime ? false : true"
-        />
+        <label class="font-semibold" for="line">Line: </label>
+        <select
+          class="ml-2 py-0 pl-3 pr-9 border border-gray-600 focus:ring-1 focus:ring-gray-600 rounded"
+          name="sslno"
+          id="line"
+        >
+          <option value="">Any Line...</option>
+          <option value="">Line-1</option>
+          <option value="">Line-2</option>
+        </select>
       </div>
     </div>
-    <div class="flex items-center">
+    <div class="flex items-center space-x-2">
       <div class="flex items-center">
+        <label class="font-semibold" for="tagno">Tag No: </label>
         <input
-          type="checkbox"
-          class="rounded-sm"
-          id="checkdatetime"
-          v-model="toggleDateTime"
-        />
-        <label class="ml-1 mr-2 font-sans font-medium" for="checkdatetime"
-          >Tag Time</label
-        >
-      </div>
-      <div class="flex items-center">
-        <input
-          class="flex-1 p-0 pl-1 border rounded"
-          :class="toggleDateTime ? null : 'opacity-5'"
-          type="datetime-local"
-          name="form"
-          id="form"
-          :disabled="toggleDateTime ? false : true"
+          type="text"
+          class="ml-2 py-0 w-1/2 pl-3 pr-9 border border-gray-600 focus:ring-1 focus:ring-gray-600 rounded"
         />
       </div>
-      <div>-</div>
       <div class="flex items-center">
+        <label class="font-semibold" for="emp">Emp No: </label>
         <input
-          class="flex-1 p-0 pl-1 border rounded"
-          :class="toggleDateTime ? null : 'opacity-5'"
-          type="datetime-local"
-          name="form"
-          id="form"
-          :disabled="toggleDateTime ? false : true"
+          type="text"
+          class="ml-2 w-1/2 py-0 pl-3 pr-9 border border-gray-600 focus:ring-1 focus:ring-gray-600 rounded"
         />
       </div>
     </div>
   </div>
 </template>
 <script>
+//import axios from "axios";
 export default {
   data() {
     return {
       toggleDate: false,
       toggleDateTime: false,
+      toDate: null,
+      fromDate: null,
+      toDateTime: null,
+      fromDateTime: null,
     };
+  },
+  methods: {
+    //
   },
 };
 </script>
-
-<style scoped>
-.form-checkbox {
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  -webkit-print-color-adjust: exact;
-  color-adjust: exact;
-  display: inline-block;
-  vertical-align: middle;
-  background-origin: border-box;
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  flex-shrink: 0;
-  height: 1em;
-  width: 1em;
-  color: #42d9e1;
-  background-color: #fff;
-  border-color: #e2e8f0;
-  border-width: 1px;
-  border-radius: 0.25rem;
-}
-
-.form-checkbox:checked {
-  background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
-  border-color: transparent;
-  background-color: currentColor;
-  background-size: 100% 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-}
-</style>
--- src/views/DailyInquery.vue
+++ src/views/DailyInquery.vue
@@ -2,7 +2,12 @@
   <app-layout>
     <div class="flex flex-col w-full bg-white">
       <InqueryForm />
-      <div class="p-3 overflow-y-scroll"></div>
+      <div class="p-3 overflow-y-scroll">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
+        reprehenderit voluptatem necessitatibus repellat excepturi culpa
+        nesciunt eum laborum distinctio repellendus, harum sapiente quam
+        dignissimos beatae pariatur. Animi in rem nostrum?
+      </div>
     </div>
   </app-layout>
 </template>
